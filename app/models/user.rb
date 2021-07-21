c86f4c253148893a32f723947f3aeeb296960381
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-07-22 00:01:39 +0900

    userモデルバリデーション記述、新規＆ログイン画面viewファイル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  # validates :email, presence: true, unique: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_zen_kana, presence: true
+  validates :last_name_zen_kana, presence: true
+  validates :birth_day, presence: true
 end
