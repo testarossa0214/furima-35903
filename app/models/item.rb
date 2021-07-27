4028c20f3cea9dec2a350f790559898fb4018a90
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-07-28 00:37:35 +0900

    アソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  # has_one :order
+  belongs_to :user
+  # has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  # has_many :order
+  # has_many :comments
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
