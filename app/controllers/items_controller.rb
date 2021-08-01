2614863228c09abc6b13430ef9aee4818f9b8354
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-08-02 08:59:57 +0900

    ActiveHash実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product, :product_description, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_date_id, :price)
+  end
 end
+# .merge(:user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   # has_one :order
   belongs_to :user
   # has_many :comments
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :categories
+  has_many :statuses
+  has_many :postages
+  has_many :prefectures
+  has_many :shipping_dates
+
+  validates :product, :product_description, :price, presence: true
+
+  validates :category, :status, :postage, :prefecture, :shipping_date, numericality: { other_than: 1 , message: "can't be blank" }
+
 end
