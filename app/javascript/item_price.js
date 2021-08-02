8420daefc1500b9881f88d1323b84aa4d730a40b
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-08-03 00:22:36 +0900

    価格表示(JS記述)実装、画像バリデーション実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const tax = Math.floor(inputValue * 0.1)
+      addTaxDom.innerHTML = tax;
+
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(inputValue - tax);
+  });
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :product, :product_description, presence: true
+  validates :product, :product_description, :image, presence: true
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ }
 
   validates :category_id, :status_id, :postage_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" }
