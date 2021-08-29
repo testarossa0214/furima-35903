5e5e2554127b48b1d0b827aa1356922785edb96e
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-08-30 02:18:49 +0900

    scrollify失敗

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("../item_price")
 require("../card")
 require("../preview")
 require('jquery')
-
+require('../scrollify')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
